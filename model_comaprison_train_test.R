22e4b4b5260e6d83402058e05c9cc6b6140b9f90
Raphael Perla <Raphael.Perla@uibk.ac.at>
2024-09-18 15:46:24 +0200

    fixed some stuff

--- model_comaprison_train_test.R
+++ model_comaprison_train_test.R
@@ -25,6 +25,7 @@ compute_ppd <- function(log_lik) {
 create_stratified_split <- function(data, sim_matrix, test_percent = 0.2) {
   # Omit NAs
   d <- na.omit(data)
+  n_participants <- length(unique(d$participant))
   
   # Sample target_test_size of participants, profiles, and items
   test_participants <- sample(unique(d$participant), size = ceiling(n_participants * test_percent))
@@ -49,7 +50,6 @@ if (!dir.exists(out_path)) {
 }
 
 groups <- list.files(path_to_models, full.names = F)
-groups <- groups[-length(groups)]
 models <- list.files(file.path(path_to_models, groups[1]), full.names = F)
 
 # Load data
@@ -68,8 +68,8 @@ subsets <- list("hc_on_hc" = subset(d, type == "HC" & profile_type == "healthy")
 
 # Initialize empty storage matrix for LOOIC
 loo_results_objects <- list()
-looic <- matrix(NA, nrow = length(groups) * length(models), ncol = 4)
-colnames(looic) <- c("group", "model", "looic", "mae")
+looic <- matrix(NA, nrow = length(groups) * length(models), ncol = 3)
+colnames(looic) <- c("group", "model", "looic")
 
 # Loop through groups
 for (i in seq_along(groups)) {
@@ -77,8 +77,8 @@ for (i in seq_along(groups)) {
   loo_objects <- list()
   current_group <- groups[i]
   
-  current_split <- create_stratified_split(subsets[[current_group]], sim_mat, .2)
-  current_test <- split$test
+  #current_split <- create_stratified_split(subsets[[current_group]], sim_mat, .2)
+  #current_test <- current_split$test
   
   # Loop through models
   for (j in seq_along(models)) {
@@ -91,38 +91,42 @@ for (i in seq_along(groups)) {
     current_model_fit <- read_rds(model_path)
     
     # Get predictions, target and likelihood
-    preds <- extract(current_model_fit, pars = "y_pred")$y_pred
-    target <- current_test$expectation
+    preds <- extract(current_model_fit, pars = "y_rep")$y_rep
+    #target <- current_test$expectation
     log_lik <- extract(current_model_fit, pars = "log_lik")$log_lik
     
     # Calculate LOOIC
     ppd <- compute_ppd(log_lik)
     elpd_estimate <- mean(log(ppd))
-    looic <- -2 * elpd_estimate
-    
-    # Calculate MAE
-    mae <- mean(apply(preds, 2, function(x) mean(abs(x - target))))
+    looic_value  <- -2 * elpd_estimate
     
     # Remove model from memory
     rm(current_model_fit)
     gc()
     
     # Store everything in the matrix
-    looic[(i - 1) * length(models) + j, ] <- c(current_group, current_model, looic, mae)
+    looic[(i - 1) * length(models) + j, ] <- c(current_group, current_model, looic_value)
   }
-  
-  current_group_result <- loo_compare(loo_objects[[models[1]]], loo_objects[[models[2]]], 
-                                      loo_objects[[models[3]]], loo_objects[[models[4]]], 
-                                      loo_objects[[models[5]]], loo_objects[[models[6]]])
-  
-  loo_results_objects[[current_group]] <- current_group_result
 }
 
 # Convert looic matrix to data frame
 looic <- as.data.frame(looic)
-looic$se <- as.numeric(looic$se)
 looic$looic <- as.numeric(looic$looic)
 
 # Save results
 write_rds(loo_results_objects, paste0(out_path, "/model_comparisons_lisa.rds"), compress = "gz")
 write_rds(looic, paste0(out_path, "/looic_lisa.rds"), compress = "gz")
+
+looic_df <- looic %>%
+  group_by(group) %>%
+  mutate(looic = looic - min(looic)) %>%
+  ungroup()
+
+# Plot barplot of LOOIC by model
+ggplot(looic_df, aes(y = reorder(model, looic), x = looic)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  facet_wrap(~group, scales = "free") +
+  labs(title = "LOOIC by model",
+       x = "Model",
+       y = "LOOIC") +
+  jtools::theme_apa()
